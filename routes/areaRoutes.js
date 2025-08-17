47e3de088be73b987fbbe9766f03b17569775d3e
Ali Madan <alimadan4321@gmail.com>
2025-08-17 10:46:09 +0300

    add the property route and fix some thigs

--- routes/areaRoutes.js
+++ routes/areaRoutes.js
@@ -3,12 +3,12 @@ const express = require("express")
 
 const router = express.Router()
 
+const areaController = require("../Controllers/propertyAreas")
+
 router.post("/new", areaController.createArea)
 
 
 router.put('/:id', areaController.updateArea)
-
-
 
 
 
